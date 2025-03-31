d2d17c2dcdd8aa1e30869fcb7ca9787a73ea1a87
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-31 21:22:18 +0100

    feat: Add project boilerplate

---
+++ ios/ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Oliver Keefe on 31/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
