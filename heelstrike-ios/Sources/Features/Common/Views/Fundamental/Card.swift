abf25353ce8a71add7b12dbc706dc30ac54547b4
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-10 22:14:13 +0100

    fix(Common): Resolved inset issue, correct padding now applied

--- heelstrike-ios/Sources/Features/Common/Views/Fundamental/Card.swift
+++ heelstrike-ios/Sources/Features/Common/Views/Fundamental/Card.swift
@@ -1,15 +1,33 @@
 import SwiftUI
 
 struct Card<Content: View>: View {
+    let title: String
     let content: () -> Content
-    
+
     var body: some View {
-        VStack {
-            content()
+        ZStack {
+            VStack(spacing: 16) {
+                HStack {
+                    Text(title)
+                        .font(.headline)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 10)
+
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(.gray)
+                        .padding(.top, 10)
+                }
+                .padding(10)
+                content()
+            }
+            .background(Color.white)
+            .cornerRadius(10)
         }
-        .padding(20)
-        .frame(maxWidth: UIScreen.main.bounds.width - 24)
-        .background(Color(.systemGray6))
-        .cornerRadius(10)
+        .frame(minWidth: 340)
+        .padding(.top, 14)
+        .padding(.leading, 24)
+        .padding(.trailing, 24)
+        .padding(.bottom, 0)
     }
 }
+
